acff8838ef23356b5924db8cfddef23a3dcae870
kothavamsi <kothavamsikrishna@gmail.com>
2016-12-04 14:32:54 +0530

    ...

--- ProductsEStore/ExceptionFilters/ExceptionFilter.cs
+++ ProductsEStore/ExceptionFilters/ExceptionFilter.cs
@@ -5,14 +5,17 @@ using System.Web;
 using System.Web.Mvc;
 using ProductsEStore.Models;
 using ProductsEStore.Controllers;
+using ProductsEStore.Core;
+using ProductsEStore.LogHandler;
 
 namespace ProductsEStore.ExceptionFilters
 {
     class RequestCriteriaException : Exception
     {
-        public RequestCriteriaException()
+        public RequestCriteria RequestCriteria { get; set; }
+        public RequestCriteriaException(RequestCriteria reqCriteria)
         {
-
+            RequestCriteria = reqCriteria;
         }
     }
 
@@ -22,13 +25,27 @@ namespace ProductsEStore.ExceptionFilters
         {
             if (!filterContext.ExceptionHandled && filterContext.Exception is RequestCriteriaException)
             {
+                var msg = string.Format("Request Criteria Exception:{0}",
+                    ((RequestCriteriaException)filterContext.Exception).RequestCriteria.SeoFriendlyCategoryName.ToString());
+                LogManager.Write(msg);
+
                 filterContext.Result = new ViewResult()
                 {
-                    //ViewData = new ViewDataDictionary<ViewModelBase>(new MyBaseController().ViewModelBaseObj),
-                    //ViewName = "error"
+                    ViewData = new ViewDataDictionary<ViewModelBase>(new Error(filterContext.Exception)),
+                    ViewName = "Error"
                 };
                 filterContext.ExceptionHandled = true;
             }
+            else
+                if (!filterContext.ExceptionHandled && filterContext.Exception is Exception)
+                {
+                    filterContext.Result = new ViewResult()
+                    {
+                        ViewData = new ViewDataDictionary<ViewModelBase>(new Error(filterContext.Exception)),
+                        ViewName = "Error"
+                    };
+                    filterContext.ExceptionHandled = true;
+                }
         }
     }
 }
