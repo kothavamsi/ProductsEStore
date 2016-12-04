ee525a0ce5042e8ec42e42aff8ece2eefcd93004
kothavamsi <kothavamsikrishna@gmail.com>
2016-12-04 14:27:50 +0530

    added error.cs

---
+++ ProductsEStore/Models/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsEStore.Models
+{
+    public class Error : ViewModelBase
+    {
+        public Exception exception { get; set; }
+
+        public Error()
+        {
+            exception = new Exception();
+        }
+        public Error(Exception e)
+        {
+            exception = e;
+        }
+    }
+}
